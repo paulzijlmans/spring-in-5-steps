81c863e37882e059be5cc9d0ccf7b715400494d8
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-06-15 21:23:48 +0200

    Spring Unit Test with a Java Context

--- src/test/java/nl/paulzijlmans/springin5steps/SpringIn5StepsBasicApplicationTests.java
+++ src/test/java/nl/paulzijlmans/springin5steps/SpringIn5StepsBasicApplicationTests.java
@@ -1,9 +1,8 @@
 package nl.paulzijlmans.springin5steps;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringIn5StepsBasicApplicationTests {
 
     @Test
